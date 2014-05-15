ead935d887904c3fd2f60dcc4c99a784bf285619
jrmeschkat <robert.meschkat@googlemail.com>
2014-05-15 17:15:24 +0200

    Simplified testing project

--- TestingProject/src/de/unikassel/meschkat/robert/WebcamPanelExample.java
+++ TestingProject/src/de/unikassel/meschkat/robert/WebcamPanelExample.java
@@ -1,27 +1,42 @@
 package de.unikassel.meschkat.robert;
-import java.awt.Dimension;
+//import java.awt.Dimension;
+import java.io.File;
+import java.io.IOException;
 
-import javax.swing.JFrame;
+import javax.imageio.ImageIO;
+//import javax.swing.JFrame;
 
 import com.github.sarxos.webcam.Webcam;
-import com.github.sarxos.webcam.WebcamPanel;
+//import com.github.sarxos.webcam.WebcamPanel;
 
 //An example from webcam-capture git
 
 public class WebcamPanelExample {
 
-	public static void main(String[] args) throws InterruptedException {
-
+	public static void main(String[] args) 
+//			throws InterruptedException 
+	{
+		System.out.println("get webcam");
 		Webcam webcam = Webcam.getDefault();
-		webcam.setViewSize(new Dimension(640, 480));
-		WebcamPanel panel = new WebcamPanel(webcam);
-		panel.setFPSDisplayed(true);
-
-		JFrame window = new JFrame("Test webcam panel");
-		window.add(panel);
-		window.setResizable(true);
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.pack();
-		window.setVisible(true);
+		System.out.println("opening webcam");
+		webcam.open();
+		System.out.println("try getting image");
+		try {
+			ImageIO.write(webcam.getImage(), "PNG", new File("hello-world.png"));
+			System.out.println("got image");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+//		Webcam webcam = Webcam.getDefault();
+//		webcam.setViewSize(new Dimension(640, 480));
+//		WebcamPanel panel = new WebcamPanel(webcam);
+//		panel.setFPSDisplayed(true);
+//
+//		JFrame window = new JFrame("Test webcam panel");
+//		window.add(panel);
+//		window.setResizable(true);
+//		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		window.pack();
+//		window.setVisible(true);
 	}
 }
