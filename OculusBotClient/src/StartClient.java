9ba7df47d341d481dd37fadaf55edc6cb1908d4e
jrmeschkat <robert.meschkat@googlemail.com>
2014-06-12 15:57:03 +0200

    fix a bug (client can now recieve frame)

--- OculusBotClient/src/StartClient.java
+++ OculusBotClient/src/StartClient.java
@@ -1,5 +1,6 @@
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
@@ -27,15 +28,13 @@ public class StartClient {
 	public StartClient(){
 		try {
 			socket = new Socket(HOST, PORT);
-			BufferedInputStream in = new BufferedInputStream(socket.getInputStream());
-			int rows = in.read();
-			int cols = in.read();
-			int type = in.read();
-			int size = in.read();
-			byte[] data = new byte[size];
-			for(int i = 0; i < data.length; i++){
-				data[i] = (byte)in.read();
-			}
+			DataInputStream in = new DataInputStream(socket.getInputStream());
+			int rows = in.readInt();
+			int cols = in.readInt();
+			int type = in.readInt();
+			int length = in.readInt();
+			byte[] data = new byte[length];
+			in.readFully(data);
 			Mat frame = new Mat(rows, cols, type);
 			frame.put(0, 0, data);
 			Highgui.imwrite("frame.jpg", frame);
@@ -45,11 +44,11 @@ public class StartClient {
 		} catch (IOException e) {
 			e.printStackTrace();
 		} 
-//		finally{
-//			if(socket != null){
-//				try {socket.close();} catch (IOException e) {}
-//			}
-//		}
+		finally{
+			if(socket != null){
+				try {socket.close();} catch (IOException e) {}
+			}
+		}
 	}
 
 }
