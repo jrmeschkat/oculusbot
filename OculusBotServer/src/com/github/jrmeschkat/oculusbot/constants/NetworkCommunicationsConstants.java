e874c1f4434c0fed2e03f55b43cee8ffb880ca6f
jrmeschkat <robert.meschkat@googlemail.com>
2016-07-07 14:01:58 +0200

    * deleted old versions

--- OculusBotServer/src/com/github/jrmeschkat/oculusbot/constants/NetworkCommunicationsConstants.java
+++
@@ -1,8 +0,0 @@
-package com.github.jrmeschkat.oculusbot.constants;
-
-
-public interface NetworkCommunicationsConstants {
-	public static final String REQUEST_CAMERA_DATA = "REQUEST_CAMERA_DATA";
-	public static final String END_CONNECTION = "END_CONNECTION";
-	public static final String DONE = "DONE";
-}
